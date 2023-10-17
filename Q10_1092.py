8bebd9ff2e76e9729484b81230c1597b8a0cd994
XBastille <148121344+XBastille@users.noreply.github.com>
2023-10-17 22:33:23 +0530

    Add files via upload

---
+++ Q10_1092.py
@@ -0,0 +1,22 @@
+def partition(num): 
+    total=(n*(n + 1))//2
+    if total%2!=0:
+        print("NO")
+    else:
+        print("YES")
+        p=total//2
+        l1=[]
+        l2=[]
+        cs=0
+        for i in range(n, 0, -1):
+            if cs+i<=p:
+                l1.append(i)
+                cs+=i
+            else:
+                l2.append(i)
+        print(len(l1))
+        print(" ".join(map(str, sorted(l1))))
+        print(len(l2))
+        print(" ".join(map(str, sorted(l2))))
+n=int(input())
+partition(n)
