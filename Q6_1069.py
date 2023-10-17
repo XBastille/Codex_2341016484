05c06f8b054f7dd2a7deb1c627ab6e60ddf71aef
XBastille <148121344+XBastille@users.noreply.github.com>
2023-10-17 12:57:28 +0530

    Add files via upload

---
+++ Q6_1069.py
@@ -0,0 +1,20 @@
+def repetitions(chara):
+    count=1  
+    max_count=1
+    for i in range(len(chara)):
+        try:
+            if chara[i+1]==chara[i] :
+                count+=1
+            else:
+                count=1
+                prev_char=chara[i]
+        except IndexError:
+            pass
+        max_count=max(max_count, count)
+    return max_count
+s=input()
+r=repetitions(s)
+print(r)
+
+
+        
